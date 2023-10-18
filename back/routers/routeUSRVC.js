68c6996865ee70b9fcb586a55cf431e14d8d4d6c
Francisco <mau910462@gmail.com>
2023-10-18 03:47:46 -0600

    Fixed Users

--- back/routers/routeUSRVC.js
+++ back/routers/routeUSRVC.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUSRV,loginUser,getAllUsers} from '../controllers/controllerUSRVC.js';
+import { createUSRV,loginUser,getAllUsers,getUserInnerP,updateU} from '../controllers/controllerUSRVC.js';
 import {registerOut} from '../controllers/controllerEntradas.js'
 
 const usrvroute = express.Router();
@@ -8,7 +8,8 @@ usrvroute.post('/', createUSRV);
 usrvroute.post('/login', loginUser);
 usrvroute.post('/logout', registerOut)
 usrvroute.get('/getAll', getAllUsers);
-
+usrvroute.get('/IP', getUserInnerP);
+usrvroute.put('/:IDUsuarios', updateU);
 
 
 export default usrvroute
