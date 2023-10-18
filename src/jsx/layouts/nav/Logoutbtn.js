bfd79d368a0b3a3a77bc9f138c8aa90429d42d57
Ajpop <ajpop_chuc@mesoamericana.edu.gt>
2023-10-18 00:56:32 -0600

    Limpieza codigo anterior4

--- src/jsx/layouts/nav/Logoutbtn.js
+++ src/jsx/layouts/nav/Logoutbtn.js
@@ -4,6 +4,7 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Logout } from '../../../store/actions/AuthActions';
 import { isAuthenticated } from '../../../store/selectors/AuthSelectors';
 import { SVGICON } from '../../constant/theme';
+import axios from 'axios';
 
 function withRouter(Component) {
   function ComponentWithRouterProp(props) {
@@ -26,12 +27,24 @@ function LogoutPage(props) {
   const navigate = useNavigate();
 
   function onLogout() {
+    //Registrar Salida en bd
+    callAPIlogout();
     // Eliminar el token del localStorage
     localStorage.removeItem('userDetails');
 
     // Luego, realizar la acción de cierre de sesión
     dispatch(Logout(navigate));
   }
+
+  function callAPIlogout() {
+    const tokenData = JSON.parse(localStorage.getItem('userDetails'));
+    const requestData = { localId: tokenData.localId };
+    console.log(tokenData.localId);
+    return axios.post(S
+        `http://127.0.0.1:3001/api/Usrv/logout`,
+        requestData,
+    );
+}
 
   return (
     <>
