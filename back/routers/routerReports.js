2c5b18bb3e39ee1a8ee72533161e3c25702cd4b6
Ajpop <ajpop_chuc@mesoamericana.edu.gt>
2023-10-17 18:45:10 -0600

    comentarios en reportes

--- back/routers/routerReports.js
+++ back/routers/routerReports.js
@@ -5,17 +5,17 @@ from '../controllers/controllerReports.js'
 
 const routerReports = express.Router()
 
-routerReports.get('/getExpiredLots', getExpiredLots)
-routerReports.get('/getSalesInner', getSalesInner)
-routerReports.get('/getMoneySoldMonth', getMoneySoldMonth)
-routerReports.get('/getMoneySoldDAY', getMoneySoldDAY)
-routerReports.get('/getMoneySoldWEEK', getMoneySoldWEEK)
-routerReports.get('/getOrderSellersMorSales', getOrderSellersMorSales)
-routerReports.get('/getSellersMorSalesMonth', getSellersMorSalesMonth)
-routerReports.get('/getOrderClientsMorSales', getOrderClientsMorSales)
-routerReports.get('/getClientsMorSalesMonth', getClientsMorSalesMonth)
-// routerReports.get('/getMoneySoldDAY', getMoneySoldDAY)
-// routerReports.get('/getMoneySoldWEEK', getMoneySoldWEEK)
+routerReports.get('/getExpiredLots', getExpiredLots) //lotes expirados
+routerReports.get('/getSalesInner', getSalesInner) // ventas, pero sin llaves primarias, ya con datos
+routerReports.get('/getMoneySoldMonth', getMoneySoldMonth)//dinero vendido mes
+routerReports.get('/getMoneySoldDAY', getMoneySoldDAY)// dinero vendido dia
+routerReports.get('/getMoneySoldWEEK', getMoneySoldWEEK)//dinero vendido semana
+routerReports.get('/getOrderSellersMorSales', getOrderSellersMorSales) //vendedores ordenados con mas ventas
+routerReports.get('/getSellersMorSalesMonth', getSellersMorSalesMonth) // vendedoers con masvetnas mes
+routerReports.get('/getOrderClientsMorSales', getOrderClientsMorSales) // clientes ordenados con mas compras
+routerReports.get('/getClientsMorSalesMonth', getClientsMorSalesMonth) //clientes con mas compras mes 
+// routerReports.get('/getMoneySoldDAY', getMoneySoldDAY) //Auditoria
+// routerReports.get('/getMoneySoldWEEK', getMoneySoldWEEK) //control entradas y salidas
 // routerReports.get('/getOrderSellersMorSales', getOrderSellersMorSales)
 
 export default routerReports
